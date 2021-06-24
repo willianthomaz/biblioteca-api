df83edc38e00afe1e72cc36c08a277404c810771
Willian Thomaz <willian_thomaz@outlook.com>
2021-06-23 22:54:22 -0300

    Ajuste da application.properties

--- src/main/java/com/willianthomaz/bibliotecaapi/BibliotecaApiApplication.java
+++ src/main/java/com/willianthomaz/bibliotecaapi/BibliotecaApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+@EnableScheduling // agendamento de tarefas
 public class BibliotecaApiApplication {
 
 	@Bean
