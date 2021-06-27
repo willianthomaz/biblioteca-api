06024bf7e163c6d92a07d0a8b7595acc8cc2ae75
Willian Thomaz <willian_thomaz@outlook.com>
2021-06-27 15:02:40 -0300

    Atualização da classe LivroDTO

--- src/main/java/com/willianthomaz/bibliotecaapi/api/dto/LivroDTO.java
+++ src/main/java/com/willianthomaz/bibliotecaapi/api/dto/LivroDTO.java
@@ -2,15 +2,19 @@ package com.willianthomaz.bibliotecaapi.api.dto;
 
 import lombok.*;
 
-@Getter
-@Setter
+import javax.validation.constraints.NotEmpty;
+
+@Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class LivroDTO {
 
     private Long id;
+    @NotEmpty
     private String titulo;
+    @NotEmpty
     private String autor;
+    @NotEmpty
     private String isbn;
 }
